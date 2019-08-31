08c9c83d0b8054fb9e3e04b23274451e8a62d6b6
od_1969 <mihanada0506s@gmail.com>
2019-09-01 02:10:36 +0900

    Change file

--- config.h
+++ config.h
@@ -201,4 +201,6 @@
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 1
 
+#define CATERINA_BOOTLOADER
+
 #endif
--- rules.mk
+++ rules.mk
@@ -72,6 +72,8 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 
+BOOTLOADER = caterina
+
 PS2_MOUSE_ENABLE = yes
 #PS2_USE_BUSYWAIT = yes
 PS2_USE_INT = yes
